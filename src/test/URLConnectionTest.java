bcc114e174244cabebaab7bfbb128a3b74d2b62a
Liam Noonan <contactliamnoonan@gmail.com>
2018-06-08 22:57:42 +0100

    removed methods from test class for now.

--- src/test/URLConnectionTest.java
+++ src/test/URLConnectionTest.java
@@ -9,21 +9,4 @@ public class URLConnectionTest {
     @org.junit.After
     public void tearDown() throws Exception {
     }
-//
-//    @org.junit.Test
-//    public void login() throws IOException, JSONException {
-//        if(URLConnection.USER("Test User","password")){
-//            System.out.println("LOGGED IN");
-//        }else{
-//            System.out.println("LOGIN FAILED");
-//        }
-//    }
-
-//    @org.junit.Test
-//    public void getUsers() throws IOException, JSONException {
-//        ArrayList<User> array = URLConnection.getUsers();
-//        for(int i =0; i<array.size();i++){
-//            System.out.println(array.get(i).getUsername());
-//        }
-//    }
 }
